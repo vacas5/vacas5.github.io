02199cbaca9d088a4a70dd5cc05685199a1e2caf
vacas5 <russell@tandum.com>
2018-01-15 10:02:55 -0600

    Use published npm package for unsplash

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,50 +3,6 @@ const Promise = require("bluebird")
 const path = require("path")
 const select = require(`unist-util-select`)
 const fs = require(`fs-extra`)
-const axios = require(`axios`)
-const crypto = require(`crypto`)
-
-exports.sourceNodes = ({ boundActionCreators }) => {
-    const { createNode } = boundActionCreators
-    return axios.get(`https://api.unsplash.com/users/bojackson30/collections`, {
-        params: {
-            client_id: process.env.UNSPLASH_APP_ID
-        }
-    }).then(response => {
-        return Promise.all(response.data.map(collection => {
-            return axios.get(collection.links.photos, {
-                params: {
-                    client_id: process.env.UNSPLASH_APP_ID
-                }
-            }).then(res => {
-                res.data.map(photo => {
-                    const digest = crypto
-                        .createHash(`md5`)
-                        .update(JSON.stringify(photo))
-                        .digest(`hex`)
-
-                    const node = Object.assign(
-                        photo,
-                        {
-                          parent: `__SOURCE__`,
-                          children: [],
-                          internal: {
-                            type: `UnsplashPhoto`,
-                            contentDigest: digest,
-                            mediaType: `application/json`
-                          },
-                        }
-                    )
-
-                    createNode(node)
-                    return true;
-                })
-            })
-        }));
-    }).catch(error => {
-        console.log(error);
-    });
-}
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators
